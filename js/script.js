c04ef2037fde517693f2f5c335aa51f499e72e91
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-12-15 22:33:17 +0900

    一括削除機能

--- js/script.js
+++ js/script.js
@@ -37,14 +37,27 @@
         if (confirm('are you sure?')) {
           this.todos.splice(index, 1);
         }
+      },
+      purge: function (index) {
+        if (!confirm('delet finishd?')) {
+          return;
+        }
+        // this.todos = this.todos.filter(function (todo) {
+        //   return !todo.isDone;
+        // });
+        this.todos = this.remaining;
       }
     },
     computed: {
       remaining: function () {
-        var items = this.todos.filter(function (todo) {
+        // var items = this.todos.filter(function (todo) {
+        //   return !todo.isDone;
+        // });
+        // return items.length;
+        return this.todos.filter(function (todo) {
           return !todo.isDone;
-        });
-        return items.length;
+        }
+        )
       }
     }
   });

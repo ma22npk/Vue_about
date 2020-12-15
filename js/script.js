bf9cf684dc37949d93c1c38f8530486a1717f2c5
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-12-15 21:26:15 +0900

    完了したタスクを分母分子で表示

--- js/script.js
+++ js/script.js
@@ -21,7 +21,6 @@
           title: 'task 3',
           isDone: true
         }
-
       ]
     },
     methods: {
@@ -38,10 +37,15 @@
         if (confirm('are you sure?')) {
           this.todos.splice(index, 1);
         }
-
       }
-
-
+    },
+    computed: {
+      remaining: function () {
+        var items = this.todos.filter(function (todo) {
+          return !todo.isDone;
+        });
+        return items.length;
+      }
     }
   });
 

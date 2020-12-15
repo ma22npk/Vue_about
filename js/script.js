cc068a723a8f5af275c649681fcd705531282d9d
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-12-15 19:26:48 +0900

    v-onイベント設定

--- js/script.js
+++ js/script.js
@@ -9,9 +9,17 @@
     el:'#app',
     //name とキーで保持
     data: {
+      newItem:'',
       todos: [
         'task 1','task 2','task 3'
       ]
+    },
+    methods: {
+      addItem: function () {
+
+        this.todos.push(this.newItem);
+        this.newItem = ''
+      }
     }
   });
 
